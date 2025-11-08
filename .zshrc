4c7495bf2453fad48e86058ceb87735c4597cd98
ItabashiHiroyuki <itabashi.hiroyuki@xmile.co.jp>
2025-11-08 13:31:22 +0900

    simplize

---
+++ .zshrc
@@ -0,0 +1,6 @@
+# Enable fzf
+source /usr/share/fzf/shell/key-bindings.zsh
+source /usr/share/fzf/shell/completion.zsh
+
+# Configure fzf history search (Ctrl+R)
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-set -u
-THIS_DIR=$(cd $(dirname $0); pwd)
-cd $THIS_DIR
-git submodule init
-git submodule update
+# Install fzf using apt-get
+sudo apt-get update
+sudo apt-get install -y fzf
 
-for f in _??*
-  do
-#   echo "$f"
-     [[ "$f" == ".git" ]] && continue
-     [[ "$f" == ".DS_Store" ]] && continue
-     b=`echo $f | sed -e "s/_\(.*\)/\.\1/"`
-#    echo "$b"
-     ln -snfv ~/dotfiles/"$f" ~/"$b"
-#     echo "$f" | sed -e "s/_\(.*\)/\.\1/"
- done
+echo "fzf installation completed!"
